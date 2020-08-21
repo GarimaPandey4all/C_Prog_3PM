b827aba3499cb1af44305d3dcd8031374c8a2828
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2020-08-21 17:44:54 +0530

    Add files via upload

---
+++ INT_SHOR.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   //signed short int x = 32769;
+   //unsigned short int x = 65538;
+   unsigned short int x = -4;
+   clrscr();
+
+   printf("The value of x is:%d\n\n", x);
+   printf("The value of x is:%u", x);
+
+   getch();
+}
